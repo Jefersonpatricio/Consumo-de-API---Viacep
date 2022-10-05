a0ced1d3cda7ef293bd848109ea2650e8c649c6f
Jeferson Patricio <jelsantos37@gmail.com>
2022-10-05 14:46:32 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,26 @@
+function consultaCep() {
+    var cep = document.getElementById("cep").value.replace(/\D/g, '');
+    var url = 'https://viacep.com.br/ws/' + cep + '/json';
+    var request = new XMLHttpRequest();
+
+
+    request.open('GET', url);
+    request.onerror = function(e) {
+        document.getElementById('return').innerHTML = 'API OFFLINE OU CEP INVÁLIDO'
+    }
+
+    request.onload = () => {
+        var response = JSON.parse(request.responseText);
+
+        if (response.erro === 'true') {
+            document.getElementById('return').innerHTML = 'Cep não encontrado.';
+        } else {
+            document.getElementById('return').innerHTML =
+                'CEP: ' + response.cep + '<br>' +
+                'Logradouro: ' + response.logradouro + '<br>' +
+                'Bairro: ' + response.bairro + '<br>' +
+                'Cidade/UF: ' + response.localidade + '/' + response.uf;
+        }
+    }
+    request.send();
+}
